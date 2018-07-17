d9758fbfb0ac5aba120561201f13c2482a4947c4
Brent Allen <carvetighter@gmail.com>
2018-07-16 22:14:31 -0500

    added four classes statements base, data, analysis, visualizations

--- code/Sp500.py
+++ code/Sp500.py
@@ -6,15 +6,13 @@
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
 
-from matplotlib import pyplot
-from matplotlib import style
-
-#import DataPrep
-import warnings
 from datetime import datetime
 from datetime import time
 from datetime import timedelta
 from datetime import timezone
+import warnings
+from matplotlib import pyplot
+from matplotlib import style
 import pandas
 import numpy
 from pandas_datareader import data
@@ -24,5 +22,14 @@ from SqlMethods import SqlMethods
 # ignore warnings
 warnings.simplefilter('ignore')
 
-class Sp500_base(object):
+class Sp500Base(object):
+    pass
+
+class Sp500Data(Sp500Base):
+    pass
+
+class Sp500Analysis(Sp500Base):
+    pass
+
+class Sp500Visualizations(Sp500Base):
     pass
